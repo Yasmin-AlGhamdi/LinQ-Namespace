ac26c91246fb540f6d5657e33506a6c24330415d
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-28 14:05:45 +0300

    Add files via upload

---
+++ LinQ_Namespace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinQ_Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<int> nums = new List<int> { 1, 22, 4, 55, 2, 6, 7 };
+            List<string> names = new List<string> { "Layan", "Lamya", "Yasmin" };
+
+            var students = from name in names
+                           where name.StartsWith('L')
+                           select name;
+            /*
+            var result = from num in nums
+                         where num > 1 //  6 & num < 22
+                         orderby num descending
+                         select num;
+            */
+
+            foreach(var res in students)
+            {
+                Console.WriteLine(res + " ");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
